e20c0ca822ecdb58f9aa04661929f34f887cce7f
gopackgo90 <cbodendein@live.com>
2018-05-19 09:15:36 -0500

    Same changes as upstream

--- scripts/optional-list.sh
+++ scripts/optional-list.sh
@@ -33,8 +33,8 @@ wait
 echo -e " ${TICK} \e[32m Removing blank lines... \e[0m"
 sudo sed -i '/^\s*$/d' /etc/pihole/whitelist.txt
 wait
-echo -e " ${TICK} \e[32m Restarting DNS service \e[0m"
-pihole restartdns > /dev/null
+echo -e " ${TICK} \e[32m Pi-hole gravity rebuilding lists... \e[0m"
+pihole -g --skip-download --whitelist-only > /dev/null
 wait
 echo -e " ${TICK} \e[32m Done! \e[0m"
 
